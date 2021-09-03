8e06c77748b250686d7c8d0eee490ac09560a975
maimai0401 <88925285+mai926@users.noreply.github.com>
2021-09-03 17:45:56 +0900

    userテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,6 @@ ActiveRecord::Schema.define(version: 2021_08_24_112858) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "name", null: false
-    t.string "nickname", null: false
-    t.string "account_id", null: false
-    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
