9e8ea57c1486847f7b67f6a00d4f502d9eb0938b
maimai0401 <88925285+mai926@users.noreply.github.com>
2021-09-06 09:39:33 +0900

    account_id一意性バリデーション付与

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,5 @@
 class Account < ApplicationRecord
   belongs_to :user, optional: true
   validates :nickname, :account_id, :birthday, presence: true
+  validate :account_id, uniqueness: true
 end
--- db/migrate/20210904040652_create_accounts.rb
+++ db/migrate/20210904040652_create_accounts.rb
@@ -2,7 +2,7 @@ class CreateAccounts < ActiveRecord::Migration[6.0]
   def change
     create_table :accounts do |t|
       t.string :nickname,               null: false
-      t.string :account_id,               null: false
+      t.string :account_id,               null: false, unique: true
       t.date :birthday,               null: false
       t.references :user
       t.timestamps
