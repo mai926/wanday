3a5408d652a35db11cbb5523e9ad5ecf6ed951ad
maimai0401 <88925285+mai926@users.noreply.github.com>
2021-09-11 22:02:22 +0900

    バリデーション修正

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,6 @@
 class Account < ApplicationRecord
   belongs_to :user, optional: true
   validates :nickname, :account_id, :birthday, presence: true
-  validates :account_id, uniqueness: true
+  # validates :account_id, uniqueness: true
+  validates :account_id, format: { with: /\A[a-zA-Z0-9]+\z/}, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :account
-
-  
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze}
   validates :name, presence: true
-
 end
