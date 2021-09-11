f5ff7ec3bb8358b35d5bace40d39a29c6b08e3ea
maimai0401 <88925285+mai926@users.noreply.github.com>
2021-09-11 22:12:36 +0900

    userモデルテスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    name {'test'}
+    email { Faker::Internet.email }
+    password { '1a' + Faker::Internet.password(min_length: 7, max_length: 20) }
+  end
+end
