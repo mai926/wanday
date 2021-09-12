b86b23adce807988fc2818765cb2ca6d429b2854
maimai0401 <88925285+mai926@users.noreply.github.com>
2021-09-12 12:44:25 +0900

    アソシエーションの記述

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,5 @@
 class Account < ApplicationRecord
   belongs_to :user, optional: true
   validates :nickname, :account_id, :birthday, presence: true
-  # validates :account_id, uniqueness: true
   validates :account_id, format: { with: /\A[a-zA-Z0-9]+\z/}, uniqueness: true, length: {minimum:6}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :account
+  has_many :posts, dependent: :destroy
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze}
   validates :name, presence: true
 end
