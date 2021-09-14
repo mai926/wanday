179fd473e0ac762412a6b8df13e9c05835c1b677
maimai0401 <88925285+mai926@users.noreply.github.com>
2021-09-14 15:45:12 +0900

    １枚のみ投稿可能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,22 @@
 class PostsController < ApplicationController
+  def index
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render 'create'
+    else
+      render 'new'
+    end  
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:caption,:image).merge(user_id: current_user.id)
+  end
 end
