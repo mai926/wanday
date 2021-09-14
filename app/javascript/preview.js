97202fb09169a5f9f7b60359491607434c6c2f0d
maimai0401 <88925285+mai926@users.noreply.github.com>
2021-09-14 16:29:10 +0900

    プレビュー機能実装

---
+++ app/javascript/preview.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const ImageList = document.getElementById("image-list");
+
+  document.getElementById("posts-image").addEventListener("change", function (e) {
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    const imageElement = document.createElement("div");
+
+    const blobImage = document.createElement("img");
+    blobImage.setAttribute("src", blob);
+    blobImage.setAttribute("class", "preview-image");
+
+    imageElement.appendChild(blobImage);
+    ImageList.appendChild(imageElement);
+  });
+});
