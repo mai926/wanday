313ab7b444e78d2279fc1d91020a6b26efc1885b
maimai0401 <88925285+mai926@users.noreply.github.com>
2021-09-15 18:19:49 +0900

    投稿機能テスト

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :post do
-    
+    caption {'test'}
+    association :user
+
+    after(:build) do |post|
+      post.images.attach(io: File.open('public/images/test_pic.png'), filename: 'test_pic.png')
+    end
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @post = FactoryBot.build(:post)
+  end
+
+  describe '投稿機能' do
+    context '投稿できる時' do
+      it '全て正常' do
+        expect(@post).to be_valid
+      end
+    end
+
+    context '投稿できない時' do
+      it 'images:必須' do
+        @post.images = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Images can't be blank")
+      end
+      it 'user:必須' do
+        @post.user = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
