97765b993a540d8380d4b0c53fa0e6a9056ddc0a
maimai0401 <88925285+mai926@users.noreply.github.com>
2021-09-20 06:57:43 +0900

    model修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many_attached :images
-
+  has_many :comments
   validates :images, presence: true
   validates :images,length: { minimum: 1, maximum: 4, message: "は1枚以上4枚以下にしてください" }
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_14_062141) do
+ActiveRecord::Schema.define(version: 2021_09_19_215317) do
 
   create_table "accounts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -43,6 +43,16 @@ ActiveRecord::Schema.define(version: 2021_09_14_062141) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "comment", null: false
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "caption", null: false
     t.bigint "user_id"
@@ -65,5 +75,7 @@ ActiveRecord::Schema.define(version: 2021_09_14_062141) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
