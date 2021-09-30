661d3db4b204fffe962d7af5607b694f0ce3caed
maimai0401 <88925285+mai926@users.noreply.github.com>
2021-09-30 22:17:58 +0900

    プルダウンメニュー表示

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start();
 // require("turbolinks").start()
 require("@rails/activestorage").start();
 require("channels");
+require("../edit_menu");
 require("../icon_hover");
 require("../preview");
 
