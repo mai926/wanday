118531100ebbb3347d050cf3d050506efd668542
maimai0401 <88925285+mai926@users.noreply.github.com>
2021-10-06 17:47:54 +0900

    モデル編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,15 @@ class User < ApplicationRecord
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze }
   validates :name, presence: true
 
+  
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # 被フォロー関係を通じて参照→followed_idをフォローしている人
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  # 与フォロー関係を通じて参照→follower_idをフォローしている人
+
+
   def update_without_current_password(params, *options)
     params.delete(:current_password)
 
@@ -23,5 +32,15 @@ class User < ApplicationRecord
 
   def liked?(post_id)
     likes.where(post_id: post_id).present?
+  end
+
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  def following?(user)
+    followings.include?(user)
   end
 end
