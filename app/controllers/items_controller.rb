9c1655cfef96620ace3b8b3254c408268f41ce4d
maimai0401 <88925285+mai926@users.noreply.github.com>
2021-10-10 23:17:59 +0900

    商品編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,12 @@
 class ItemsController < ApplicationController
+  before_action :select_item, only: [:edit, :update]
+
   def new
     @item = Item.new
   end
 
   def create 
     @item = Item.new(item_params)
-    # binding.pry
     if @item.save
       redirect_to items_path
     else
@@ -13,8 +14,21 @@ class ItemsController < ApplicationController
     end
   end
 
+  def update
+    if @item.update(item_params)
+      return redirect_to item_path(@item)
+    else
+      render 'edit'
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:name,:explanation,images:[]).merge(user_id: current_user.id)
   end
+
+  def select_item
+    @item = Item.find(params[:id])
+  end
+
 end
