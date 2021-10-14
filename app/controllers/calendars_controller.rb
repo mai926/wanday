8cb72dc25c806ce6324b98ba9cc286c34f0369c2
maimai0401 <88925285+mai926@users.noreply.github.com>
2021-10-14 21:38:22 +0900

    カレンダーコントローラーの編集

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,2 +1,44 @@
 class CalendarsController < ApplicationController
+  def index
+    @calendars = Calendars.all
+    @calendar = Calendars.new
+  end
+  
+  def new
+    @calendar = Calendars.new
+  end
+
+  def show
+    @calendar = Calendars.find(params[:id])
+  end
+
+  def create
+    Calendars.create(calendar_parameter)
+    redirect_to calendar_path
+  end
+
+  def destroy
+    @calendar = Calendars.find(params[:id])
+    @calendar.destroy
+    redirect_to calendar_path, notice:"削除しました"
+  end
+
+  def edit
+    @calendar = Calendars.find(params[:id])
+  end
+
+  def update
+    @calendar = Calendars.find(params[:id])
+    if @calendar.update(calendar_parameter)
+      redirect_to calendar_path, notice: "編集しました"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def calendar_parameter
+    params.require(:calendar).permit(:event, :text, :start_time).(user_id: current_user.id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_14_003142) do
+ActiveRecord::Schema.define(version: 2021_10_14_122638) do
 
   create_table "accounts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -42,6 +42,16 @@ ActiveRecord::Schema.define(version: 2021_10_14_003142) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "calendars", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "event", null: false
+    t.text "text", null: false
+    t.date "start_time", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_calendars_on_user_id"
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -137,6 +147,7 @@ ActiveRecord::Schema.define(version: 2021_10_14_003142) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "calendars", "users"
   add_foreign_key "comments", "accounts"
   add_foreign_key "comments", "posts"
   add_foreign_key "item_reviews", "items"
