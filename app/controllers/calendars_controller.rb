cc71ca67d18011c913467e3bf722bbc70e2f9330
maimai0401 <88925285+mai926@users.noreply.github.com>
2021-10-17 11:00:46 +0900

    コントローラー修正（作成機能実装）

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,44 +1,49 @@
 class CalendarsController < ApplicationController
   def index
-    @calendars = Calendars.all
-    @calendar = Calendars.new
+    # @calendars = Calendar.find(user_id: [*current_user])
+    @calendars = Calendar.where(user_id: [*current_user])
+    @calendar = Calendar.new
   end
   
-  def new
-    @calendar = Calendars.new
-  end
+  # def new
+  #   @calendar = Calendar.new
+  # end
 
-  def show
-    @calendar = Calendars.find(params[:id])
-  end
+  # def show
+  #   @calendar = Calendar.find(params[:id])
+  # end
 
   def create
-    Calendars.create(calendar_parameter)
-    redirect_to calendar_path
-  end
-
-  def destroy
-    @calendar = Calendars.find(params[:id])
-    @calendar.destroy
-    redirect_to calendar_path, notice:"削除しました"
-  end
-
-  def edit
-    @calendar = Calendars.find(params[:id])
-  end
-
-  def update
-    @calendar = Calendars.find(params[:id])
-    if @calendar.update(calendar_parameter)
-      redirect_to calendar_path, notice: "編集しました"
-    else
-      render 'edit'
+    @calendar = Calendar.new(calendar_parameter)
+    if @calendar.save
+      redirect_to calendars_path
     end
+    # Calendar.create(calendar_parameter)
+    # redirect_to calendars_path
   end
+
+  # def destroy
+  #   @calendar = Calendar.find(params[:id])
+  #   @calendar.destroy
+  #   redirect_to calendar_path, notice:"削除しました"
+  # end
+
+  # def edit
+  #   @calendar = Calendar.find(params[:id])
+  # end
+
+  # def update
+  #   @calendar = Calendar.find(params[:id])
+  #   if @calendar.update(calendar_parameter)
+  #     redirect_to calendar_path, notice: "編集しました"
+  #   else
+  #     render 'edit'
+  #   end
+  # end
 
   private
 
   def calendar_parameter
-    params.require(:calendar).permit(:event, :text, :start_time).(user_id: current_user.id)
+    params.require(:calendar).permit(:event, :text, :start_time).merge(user_id: current_user.id)
   end
 end
