3c8b119634a3a993726e890626238d5cf63c5654
maimai0401 <88925285+mai926@users.noreply.github.com>
2021-10-17 14:36:26 +0900

    編集機能実装

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,48 +1,41 @@
 class CalendarsController < ApplicationController
+  before_action :select_calendar, only: [:show, :edit, :update,:destroy]
+
   def index
     @calendars = Calendar.where(user_id: [*current_user])
     @calendar = Calendar.new
   end
   
-  # def new
-  #   @calendar = Calendar.new
-  # end
-
-  # def show
-  #   @calendar = Calendar.find(params[:id])
-  # end
+  def show
+  end
 
   def create
     @calendar = Calendar.new(calendar_parameter)
     if @calendar.save
       redirect_to calendars_path
     end
-    # Calendar.create(calendar_parameter)
-    # redirect_to calendars_path
   end
 
-  # def destroy
-  #   @calendar = Calendar.find(params[:id])
-  #   @calendar.destroy
-  #   redirect_to calendar_path, notice:"削除しました"
-  # end
+  def update
+    if @calendar.update(calendar_parameter)
+      redirect_to calendar_path
+    else
+      render 'show'
+    end
+  end
 
-  # def edit
-  #   @calendar = Calendar.find(params[:id])
-  # end
-
-  # def update
-  #   @calendar = Calendar.find(params[:id])
-  #   if @calendar.update(calendar_parameter)
-  #     redirect_to calendar_path, notice: "編集しました"
-  #   else
-  #     render 'edit'
-  #   end
-  # end
+  def destroy
+    @calendar.destroy
+    redirect_to calendar_path, notice:"削除しました"
+  end
 
   private
 
   def calendar_parameter
     params.require(:calendar).permit(:event, :text, :start_time).merge(user_id: current_user.id)
+  end
+
+  def select_calendar
+    @calendar = Calendar.find(params[:id])
   end
 end
