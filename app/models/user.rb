e40d3aef8897a55111cbd6b87663f25a8eb6121f
maimai0401 <88925285+mai926@users.noreply.github.com>
2021-10-29 11:04:48 +0900

    コントローラーのアクションを編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,4 +48,12 @@ class User < ApplicationRecord
   def following?(user)
     followings.include?(user)
   end
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+  end
 end
