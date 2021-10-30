90aaa6efe88f767d187e451c0f5091a2b2837f28
maimai0401 <88925285+mai926@users.noreply.github.com>
2021-10-30 10:38:57 +0900

    ルボコップ実行

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,21 +3,21 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     authorization
-   end
-  
-   def google_oauth2
+  end
+
+  def google_oauth2
     authorization
-   end
-  
-   private
-  
-   def authorization
-    sns_info = User.from_omniauth(request.env["omniauth.auth"])
+  end
+
+  private
+
+  def authorization
+    sns_info = User.from_omniauth(request.env['omniauth.auth'])
     @user = sns_info[:user]
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
     else
       render template: 'devise/registrations/new'
     end
-   end
+  end
 end
