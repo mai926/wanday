1037b989134f2eb11e037470b647f80cc1170f4c
maimai0401 <88925285+mai926@users.noreply.github.com>
2021-10-31 11:42:33 +0900

    既存のルームがある際はそちらに遷移するように修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,23 +7,6 @@ class HomeController < ApplicationController
   def show
     @account = Account.find(params[:id])
     @likes = @user.likes
-    @currentUserEntry=Entry.where(user_id: current_user.id)
-    @userEntry=Entry.where(user_id: @user.id)
-    unless @user.id == current_user.id
-      @currentUserEntry.each do |cu|
-        @userEntry.each do |u|
-          if cu.room_id == u.room_id then
-            @isRoom = true
-            @roomId = cu.room_id
-          end
-        end
-      end
-      if @isRoom
-      else
-        @room = Room.new
-        @entry = Entry.new
-      end
-    end
   end
 
   def like_list
@@ -37,6 +20,26 @@ class HomeController < ApplicationController
 
   def following
     @following_users = @user.followings
+    @currentUserEntry=Entry.where(user_id: current_user.id)
+    @userEntry=Entry.where(user_id: @user.id)
+
+    @following_users.each do |following_user|
+      unless following_user.id == current_user.id
+        @currentUserEntry.each do |cu|
+          @userEntry.each do |u|
+            if cu.room_id == u.room_id then
+              @isRoom = true
+              @roomId = cu.room_id
+            end
+          end
+        end
+        if @isRoom
+        else
+          @room = Room.new
+          @entry = Entry.new
+        end
+      end
+    end
   end
 
   def profile_index
