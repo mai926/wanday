6e51dcad864f5dabda1844310c12f8832b8ff184
maimai0401 <88925285+mai926@users.noreply.github.com>
2021-11-11 16:11:08 +0900

    メニュー実装

---
+++ app/javascript/menu.js
@@ -0,0 +1,18 @@
+"use stript";
+
+$(function () {
+  // ハンバーガーメニュークリックイベント
+  $(".header-user-icon").click(function () {
+    if ($(".nav-sp").hasClass("open")) {
+      // ナビゲーション非表示
+      $(".nav-sp").removeClass("open");
+      // ハンバーガーメニューを元に戻す
+      $(this).removeClass("open");
+    } else {
+      // ナビゲーションを表示
+      $(".nav-sp").addClass("open");
+      // ハンバーガーメニューを✖印に変更
+      $(this).addClass("open");
+    }
+  });
+});
