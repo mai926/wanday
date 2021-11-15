ea067d274479d66d1adbbe72759003c5c87c59b1
maimai0401 <88925285+mai926@users.noreply.github.com>
2021-11-15 13:46:31 +0900

    アカウント情報、ペット情報編集時のプレビュー表示

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -16,7 +16,7 @@ document.addEventListener(
         preview.appendChild(img); // #previewの中に追加
       };
 
-      // いざファイルをURLとして読み込む
+      // ファイルをURLとして読み込む
       reader.readAsDataURL(file);
     }
 
@@ -31,75 +31,3 @@ document.addEventListener(
   },
   false
 );
-// function preview() {
-//   const ImageList = document.getElementById("image-list");
-
-//   if (!ImageList) return null;
-
-//   const createImageHTML = (blob) => {
-//     const imageElement = document.createElement("div");
-//     imageElement.setAttribute("class", "image-element");
-//     let imageElementNum = document.querySelectorAll(".image-element").length;
-
-//     const blobImage = document.createElement("img");
-//     blobImage.setAttribute("src", blob);
-//     blobImage.setAttribute("class", "preview-image");
-
-//     imageElement.setAttribute("id", `item_image_${imageElementNum}`);
-
-//     imageElement.appendChild(blobImage);
-//     ImageList.appendChild(imageElement);
-//   };
-
-//   document.getElementById("item-image").addEventListener("change", function (e) {
-//     const file = e.target.files[0];
-//     console.log(e.target.files);
-
-//     const blob = window.URL.createObjectURL(file);
-
-//     createImageHTML(blob);
-//   });
-// }
-// window.addEventListener("load", preview);
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   if (document.getElementById("posts-image")) {
-//     const ImageList = document.getElementById("image-list");
-
-//     const createImageHTML = (blob) => {
-//       const imageElement = document.createElement("div");
-//       imageElement.setAttribute("class", "image-element");
-//       let imageElementNum = document.querySelectorAll(".image-element").length;
-
-//       const blobImage = document.createElement("img");
-//       blobImage.setAttribute("src", blob);
-//       blobImage.setAttribute("class", "preview-image");
-
-//       const inputHTML = document.createElement("input");
-//       inputHTML.setAttribute("id", `post_image_${imageElementNum}`);
-//       inputHTML.setAttribute("name", "post[images][]");
-//       inputHTML.setAttribute("type", "file");
-
-//       imageElement.appendChild(blobImage);
-//       imageElement.appendChild(inputHTML);
-//       ImageList.appendChild(imageElement);
-
-//       inputHTML.addEventListener("change", (e) => {
-//         // console.log("hoge");
-//         file = e.target.files[0];
-
-//         blob = window.URL.createObjectURL(file);
-
-//         createImageHTML(blob);
-//       });
-//     };
-//     // ここにfor文で繰り返し処理を書いてみる
-//     document.getElementById("posts-image").addEventListener("change", function (e) {
-//       const file = e.target.files[0];
-//       // console.log(e.target.files);
-//       let blob = window.URL.createObjectURL(file);
-
-//       createImageHTML(blob);
-//     });
-//   }
-// });
